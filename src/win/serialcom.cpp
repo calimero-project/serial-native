b434ee6c86fa36b3ab581956ff3cf62311cea3dc
bmalinowsky <bmalinowsky@github.com>
2014-09-28 17:00:24 +0200

    Change file header to LGPL

--- src/win/serialcom.cpp
+++ src/win/serialcom.cpp
@@ -1,23 +1,23 @@
 /*
     Calimero 2 - A library for KNX network access
-    Copyright (c) 2007, 2011 B. Malinowsky <calimero.project@gmail.com>
+    Copyright (c) 2007, 2014 B. Malinowsky
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
+    This library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-//  This file accompanies Calimero 2, a library for KNX network access. 
+//  This file is part of Calimero 2, a library for KNX network access.
 //  It provides Java Native Interface (JNI) access to the Microsoft Windows 
 //  serial port communication I/O API.
 //
